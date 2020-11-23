066d577ccc3afe0a0473c69b76243c288335f5f4
Aleksandar Obreshkov <aleksandar.obreshkov@sap.com>
2020-11-23 17:10:11 +0200

    Added error handling

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,14 +4,12 @@ import com.example.app.domain.Character;
 import com.example.app.domain.Starship;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.cli.*;
+import sun.awt.geom.AreaOp;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.UUID;
+import java.util.*;
 
 public class App 
 {
@@ -24,36 +22,39 @@ public class App
                         .longOpt("title")
                         .hasArg()
                         .required()
+                        .type(String.class)
                         .build();
                 Option rating = Option.builder("r")
                         .longOpt("rating")
                         .hasArg()
                         .required()
+                        .type(Float.class)
                         .build();
 
                 Option releaseDate = Option.builder("d")
                         .longOpt("release-date")
                         .hasArg()
                         .required()
+                        .type(Date.class)
                         .build();
-
                 options.addOption(title);
                 options.addOption(rating);
                 options.addOption(releaseDate);
 
-                break;
+                return options;
             }
-
             case "add-character":{
                 Option name = Option.builder("n")
                         .longOpt("name")
                         .hasArg()
                         .required()
+                        .type(String.class)
                         .build();
                 Option age = Option.builder("a")
                         .longOpt("age")
                         .hasArg()
                         .required()
+                        .type(Integer.class)
                         .build();
 
                 Option forceUser = Option.builder("f")
@@ -63,64 +64,64 @@ public class App
                 options.addOption(name);
                 options.addOption(age);
                 options.addOption(forceUser);
-                break;
+                return options;
             }
-
             case "add-starship":{
                 Option name = Option.builder("n")
                         .longOpt("name")
                         .hasArg()
                         .required()
+                        .type(String.class)
                         .build();
                 Option length = Option.builder("l")
                         .longOpt("length")
                         .hasArg()
                         .required()
+                        .type(Float.class)
                         .build();
 
                 options.addOption(name);
                 options.addOption(length);
-                break;
-
+                return options;
             }
-
         }
+
         if (mainOption.contains("delete")){
             options.addOption("id", true, "delete an item with the specified id");
+            return options;
         }
-
-
-
-        return options;
+        else {
+            System.out.printf("Non-existing command \"%s\" ", mainOption);
+            return options;
+        }
     }
 
-    public static void writeDataToFile(ArrayList data, File file){
+    public static void writeDataToFile(List data, File file) throws IOException{
         ObjectMapper mapper=new ObjectMapper();
-        try {
-            mapper.writeValue(file, data);
-        } catch (IOException e) {
-            System.out.println("IO exception");
-        }
-
+        mapper.writeValue(file, data);
     }
 
     public static void main(String[] args) {
 
-        ObjectMapper mapper = new ObjectMapper();
+        args = new String[]{"add-movie", "-t", "Hello", "-r", "r","-d", "2000-12-12"};
+
+
         //just file names
         File fileCharacters = new File("C:/Users/I542072/app/src/main/resources/dummyCharacter.json");
         File fileMovies= new File("C:/Users/I542072/app/src/main/resources/dummyMovies.json");
         File fileStarships = new File("C:/Users/I542072/app/src/main/resources/dummyStarships.json");
 
-        ArrayList<Character> characters=new ArrayList<>();
-        ArrayList<Movie> movies=new ArrayList<>();
-        ArrayList<Starship> ships=new ArrayList<>();
+        //storage for the data
+        List<Character> characters=new ArrayList<>();
+        List<Movie> movies=new ArrayList<>();
+        List<Starship> ships=new ArrayList<>();
+
+        ObjectMapper mapper = new ObjectMapper();
 
         //list init
         try {
-            //separate try-catch
-            Character[] a= mapper.readValue(fileCharacters, Character[].class);
-            characters.addAll(Arrays.asList(a));
+            //separate try-catch ?
+            characters.addAll(Arrays.asList(mapper.readValue(fileCharacters, Character[].class)));
             movies.addAll(Arrays.asList(mapper.readValue(fileMovies, Movie[].class)));
             ships.addAll(Arrays.asList(mapper.readValue(fileStarships,Starship[].class)));
 
@@ -129,7 +130,7 @@ public class App
         }
 
         CommandLineParser parser = new DefaultParser();
-        CommandLine cmd=null;
+        CommandLine cmd;
 
         if(args[0].equals("movies")&&!movies.isEmpty()) {
             for (Movie a:movies) {
@@ -148,88 +149,142 @@ public class App
         }
         else{
             Options options = optionsSetup(args[0]);
+
             try {
-                cmd = parser.parse(options,args);
+                if (options.getOptions().isEmpty())throw new ParseException("Invalid command name");
+                String[] newArgs = Arrays.copyOfRange(args, 1, args.length);
+                cmd = parser.parse(options, newArgs);
 
-                if (args[0].equals("add-movie")){
-                    Movie newMovie=new Movie(
-                            UUID.randomUUID().toString(),
-                            cmd.getOptionValue("t"),
-                            Date.valueOf(cmd.getOptionValue("d")),
-                            Float.parseFloat(cmd.getOptionValue("r")));
-                    movies.add(newMovie);
+                switch (args[0]) {
+                    case "add-movie":{
+                        try {
+                            Movie newMovie = new Movie(
+                                    UUID.randomUUID().toString(),
+                                    cmd.getOptionValue("t"),
+                                    Date.valueOf(cmd.getOptionValue("d")),
+                                    Float.parseFloat(cmd.getOptionValue("r")));
+                            movies.add(newMovie);
+                            writeDataToFile(movies, fileMovies);
 
-                    writeDataToFile(movies,fileMovies);
+                        }catch (NumberFormatException e){
+                            System.out.println("Rating should be float.");
+                        }catch (IllegalArgumentException e){
+                            System.out.println("Wrong date format.");
+                        }
+                        break;
+                    }
+                    case "add-character":{
 
-                }
-                else if(args[0].equals("add-character")){
-                    Character newCharacter = new Character(
-                            UUID.randomUUID().toString(),
-                            cmd.getOptionValue("name"),
-                            Integer.parseInt(cmd.getOptionValue("age")),
-                            false
-                    );
-                    if (cmd.hasOption("force-user")) newCharacter.setForceUser(true);
-                    characters.add(newCharacter);
+                        try{
+                            Character newCharacter = new Character(
+                                    UUID.randomUUID().toString(),
+                                    cmd.getOptionValue("name"),
+                                    Integer.parseInt(cmd.getOptionValue("age")),
+                                    false);
+                            if (cmd.hasOption("force-user")) newCharacter.setForceUser(true);
+                            characters.add(newCharacter);
 
-                    writeDataToFile(characters, fileCharacters);
-                }
-                else if(args[0].equals("add-starship")){
-                    Starship newShip= new Starship(
-                            UUID.randomUUID().toString(),
-                            cmd.getOptionValue("n"),
-                            Float.parseFloat(cmd.getOptionValue("l"))
-                    );
-                    ships.add(newShip);
-
-                    writeDataToFile(ships, fileStarships);
-                }
-
-                else if(args[0].equals("delete-movie")){
-                    String idToDelete = cmd.getOptionValue("id");
-                    int id = 0;
-                    for(Movie a:movies){
-                        if (a.getId().equals(idToDelete)){
-                            id=movies.indexOf(a);
-                            movies.remove(id);
+                            writeDataToFile(characters, fileCharacters);
                             break;
+
+                        }catch (NumberFormatException e){
+                            System.out.println("Age should be an integer.");
+                            }
                         }
-                    }
-                    writeDataToFile(movies,fileMovies);
-                }
-                else if(args[0].equals("delete-character")){
-                    String idToDelete = cmd.getOptionValue("id");
-                    int id=0;
-                    for(Character a:characters) {
-                        if (a.getId().equals(idToDelete)) {
-                            id = characters.indexOf(a);
-                            characters.remove(id);
+                    case "add-starship": {
+
+                        try {
+                            Starship newShip = new Starship(
+                                    UUID.randomUUID().toString(),
+                                    cmd.getOptionValue("n"),
+                                    Float.parseFloat(cmd.getOptionValue("l"))
+                            );
+                            ships.add(newShip);
+
+                            writeDataToFile(ships, fileStarships);
                             break;
+                        }catch (NumberFormatException e){
+                            System.out.println("Length should be float.");
                         }
+
                     }
 
+                    //too brute force, optimization possible
+                    case "delete-movie": {
+                        try{
+                            String idToDelete = cmd.getOptionValue("id");
+                            int id = 0;
+                            for (Movie a : movies) {
+                                if (a.getId().equals(idToDelete)) {
+                                    id = movies.indexOf(a);
+                                    movies.remove(id);
+                                    break;
+                                }
+                            }
+                            writeDataToFile(movies, fileMovies);
+                            break;
+                        }catch (NumberFormatException e){
+                            System.out.println("Id should be an integer.");
+                        }
 
-                    writeDataToFile(characters, fileCharacters);
-                }
-                else if(args[0].equals("delete-starship")){
-                    String idToDelete = cmd.getOptionValue("id");
-                    int id=0;
-                    for(Starship a:ships) {
-                        if (a.getId().equals(idToDelete)) {
-                            id = ships.indexOf(a);
-                            ships.remove(id);
+                    }
+                    case "delete-character": {
+
+                        try {
+                            String idToDelete = cmd.getOptionValue("id");
+                            int id = 0;
+                            for (Character a : characters) {
+                                if (a.getId().equals(idToDelete)) {
+                                    id = characters.indexOf(a);
+                                    characters.remove(id);
+                                    break;
+                                }
+                            }
+
+
+                            writeDataToFile(characters, fileCharacters);
+                            break;
+                        }catch (NumberFormatException e){
+                            System.out.println("Id should be an integer");
+                        }
+
+
+                    }
+                    case "delete-starship": {
+                        try {
+                            String idToDelete = cmd.getOptionValue("id");
+                            int id = 0;
+                            for (Starship a : ships) {
+                                if (a.getId().equals(idToDelete)) {
+                                    id = ships.indexOf(a);
+                                    ships.remove(id);
+                                }
+                            }
+                            writeDataToFile(ships, fileStarships);
+                            break;
+                        }catch (NumberFormatException e){
+                            System.out.println("Id should be an integer");
                         }
                     }
-                    writeDataToFile(ships, fileStarships);
+                    default:{
+                        System.out.println("Non-existing command.");
+                    }
                 }
-            } catch (ParseException e) {
-                e.getStackTrace();
-            } catch (InputMismatchException e){
-                System.out.println("Wrong input format");
+            //missing required option
+            }catch (MissingOptionException e){
+                //see if its possible to get the long names of the options
+                System.out.println(e.getMessage());
             }
-
-
+            //catches an unrecognised option
+            catch (UnrecognizedOptionException e){
+                System.out.println("Non-existing option "+e.getOption());
+            }
+            //default parser handler (catches UnrecognisedException if main command doesnt exist)
+            catch (ParseException e) {
+                //e.printStackTrace();
+            }catch (IOException ioe){
+                System.out.println("Error when writing data to file.");
+            }
         }
-
     }
 }
