0c2663c32886ff3770e53c8dd49fedc8e01084f0
aleksandar.obreshkov@sap.com <aleksandar.obreshkov@sap.com>
2021-03-16 15:59:02 +0200

    Removed http headers from console output

--- Client/src/main/java/com/example/app/App.java
+++ Client/src/main/java/com/example/app/App.java
@@ -6,6 +6,7 @@ import com.example.app.errors.InvalidInputException;
 import org.apache.commons.cli.ParseException;
 
 public class App {
+
     public static void main(String[] args) {
         CommandFactory factory = new CommandFactory();
         try {
