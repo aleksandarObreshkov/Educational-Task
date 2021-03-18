61bc0979888286a24b7267ca389629895398b2d7
Aleksandar Tsvetkov <aleksandar.tsvetkov@sap.com>
2021-03-18 17:48:20 +0200

    Review persistence module

--- persistence/src/main/java/repositories/DatabaseActionTemplate.java
+++ persistence/src/main/java/repositories/DatabaseActionTemplate.java
@@ -3,11 +3,14 @@ package repositories;
 import javax.persistence.*;
 import java.util.function.Function;
 
+// TODO This class can be package-private as it isn't needed outside of this package. Also, I'd prefer a name like
+// "AbstractEntityRepository" or "BaseEntityRepository" as those are common naming conventions for classes that hold
+// reusable protected methods.
 public abstract class DatabaseActionTemplate {
 
     public <T> T executeInTransaction(Function<EntityManager, T> databaseAction) {
         EntityManager manager = createEntityManager();
-        try{
+        try {
             manager.getTransaction().begin();
             T result = databaseAction.apply(manager);
             manager.getTransaction().commit();
@@ -17,19 +20,25 @@ public abstract class DatabaseActionTemplate {
             if (manager.getTransaction().isActive()){
                 manager.getTransaction().rollback();
             }
-            manager.close();
+            manager.close(); // TODO This should be done in the finally clause.
+            // TODO "Database transaction failed" is too generic for anyone to figure out what went wrong. You should
+            // append the message of the cause to add some details.
             throw new RollbackException("Database transaction failed.", e);
         }
     }
 
-    public <T> T execute(Function<EntityManager, T> databaseAction){
+    public <T> T execute(Function<EntityManager, T> databaseAction) {
         EntityManager manager = createEntityManager();
         T result = databaseAction.apply(manager);
-        manager.close();
+        manager.close(); // TODO This should be done in a finally clause in case the database action throws an
+                         // exception.
         return result;
     }
 
-    private EntityManager createEntityManager(){
+    private EntityManager createEntityManager() {
+        // TODO Creating an EntityManagerFactory instance is an expensive operation. You should keep in in a static
+        // field and reuse it instead of creating a new one every time. EntityManagers on the other hand are cheap and
+        // don't need to be reused.
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("PostgreJPA");
         return factory.createEntityManager();
     }
--- persistence/src/main/java/repositories/EntityRepository.java
+++ persistence/src/main/java/repositories/EntityRepository.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+// TODO "T" is declared in all methods. You could just add it to the class definition, so that you don't need to
+// declare it over and over again.
 public class EntityRepository extends DatabaseActionTemplate {
 
-    public <T> List<T> findAll(Class<T> type){
-        return execute(manager -> manager.createQuery("SELECT a from "+type.getSimpleName()+" a", type).getResultList());
+    public <T> List<T> findAll(Class<T> type) {
+        return execute(
+                // TODO
+                manager -> manager.createQuery("SELECT a FROM " + type.getSimpleName() + " a", type).getResultList());
     }
+
     public <T> void save(T objectToPersist) {
         executeInTransaction(manager -> {
             manager.persist(objectToPersist);
@@ -19,13 +24,15 @@ public class EntityRepository extends DatabaseActionTemplate {
 
     public <T> boolean deleteById(Long id, Class<T> type) {
         return executeInTransaction(manager -> {
-            int affectedRows = manager.createQuery("delete from "+type.getSimpleName()+" a where a.id="+id).executeUpdate();
-            return affectedRows>0;
+            // TODO
+            int affectedRows = manager.createQuery("DELETE FROM " + type.getSimpleName() + " a WHERE a.id=" + id)
+                    .executeUpdate();
+            return affectedRows > 0;
         });
     }
 
-    public <T> T findById(Long id, Class<T> type){
-        return execute( (manager) -> manager.find(type,id));
+    public <T> T findById(Long id, Class<T> type) {
+        return execute(manager -> manager.find(type, id));
     }
 
 }
